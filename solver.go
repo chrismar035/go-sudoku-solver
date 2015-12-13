6d66b7164442f6b7374182793c81072f22dd4860
Chris Marshall <chris@chrismar035.com>
2015-12-13 09:53:50 -0500

    solver has Solvers to solve puzzles
    each can use a different strategy

--- solver.go
+++ solver.go
@@ -1,30 +1,25 @@
 package solver
 
-func NewPuzzle(ints [81]int) Puzzle {
-	return puzzleFromArray(ints)
+// Grid is a set of 81 integers representing a Sudoku board. Values 1-9
+// represent given or found values. 0s represent unknowns.
+type Grid [81]int
+
+// Solver defines the interface for a Sudoku solver
+type Solver interface {
+	Solve(g Grid) Grid
 }
 
-func puzzleFromArray(ints [81]int) Puzzle {
-	return Puzzle{Initial: ints, Solution: Solve(ints)}
+// NewSolver creates a new Sudoku solver with teh default solving strategy.
+// Currently only the backtracking solver is able to solve more than the most
+// basic puzzles and so is returned.
+func NewSolver() Solver {
+	return NewBacktrackingSolver()
 }
 
-func Solve(puzzle [81]int) [81]int {
-	working := workingFromArray(puzzle)
-	blankCount := working.blankCount()
+func NewBacktrackingSolver() Solver {
+	return backtrackingSolver{}
+}
 
-	for {
-		for i, square := range working {
-			if square.Value == 0 {
-				working = processSquare(i, working)
-			}
-		}
-		newBlankCount := working.blankCount()
-		if newBlankCount == blankCount {
-			break
-		} else {
-			blankCount = newBlankCount
-		}
-	}
-
-	return working.toArray()
+func NewLogicalSolver() Solver {
+	return logicalSolver{}
 }
